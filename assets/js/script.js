c5553d98dd52778043dead5d5e685fd40bcdabbf
Matias <matias.stur@gmail.com>
2020-03-04 17:49:45 -0300

    Sexto commit contac y footer listo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,24 @@
 
 
   $(function () {
+
+    $("a").click(function(event){
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var ident = this.hash;
+
+      $("html, body").animate({
+        scrollTop: $(ident).offset().top
+      }, 800, function(){
+        window.location.hash = ident;
+      });
+    }
+  });
+
     $('[data-toggle="popover"]').popover()
+
+    $("a").attr('target','_blank');
+
+
   });
